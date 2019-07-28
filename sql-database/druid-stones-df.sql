a231b464bb8bbced3f0d2cf0d01063e75513b8af
Harrison <harrisonized@gmail.com>
2019-07-27 19:17:22 -0700

    adding sql database files

---
+++ sql-database/druid-stones-df.sql
@@ -0,0 +1,22 @@
+\c mountain_project;
+
+CREATE TABLE druid_stones_df(
+  ind INT,
+  route TEXT,
+  location TEXT,
+  url TEXT,
+  avg_stars FLOAT,
+  your_stars INT,
+  route_type VARCHAR(15),
+  rating VARCHAR(11),
+  pitches INT,
+  len FLOAT,
+  area_latitude FLOAT,
+  area_longitude FLOAT,
+  star_ratings INT,
+  suggested_ratings INT,
+  on_to_do_lists INT,
+  ticks INT
+);
+
+\copy druid_stones_df FROM '/home/harrisonized/github/mountain-project-recommender/data/scraped-data/druid_stones-df.csv' DELIMITER ',' CSV HEADER;
